ab1efa5a6092bf88c35cdacaeb28c3bc0e2c17ec
Andrew <grozzy09@gmail.com>
2022-04-20 03:25:45 -0400

    Tab pane v0.1.0

--- it-ticket/src/components/HomeComponent.js
+++ it-ticket/src/components/HomeComponent.js
@@ -1,47 +1,95 @@
-import React from 'react';
-import { Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, Card, CardTitle, CardText, Button} from 'reactstrap';
 import { FadeTransform } from 'react-animation-components';
 
-function RenderTabs() {
+function RenderTabs({handleToggle, isActive}) {
     return(
-        <div>
-        <Nav tabs>
-          <NavItem>
+        <div className="row tab-shadow">
+        <Nav justified tabs>
+            <NavItem>
             <NavLink
-              active
-              href="#"
+                className={isActive ? "active" : null}
+                onClick={handleToggle}
             >
-              Link
+                Tab1
             </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">
-              Second Link
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">
-              Third Link
-            </NavLink>
-          </NavItem>
-          <NavItem>
+            </NavItem>
+            <NavItem>
             <NavLink
-              href="#"
+                className={isActive ? null : "active"}
+                onClick={handleToggle}
             >
-              Fourth Link
+                More Tabs
             </NavLink>
-          </NavItem>
+            </NavItem>
         </Nav>
-      </div>
+        <TabContent className="mt-5" activeTab={isActive ? "1" : "2"}>
+            <TabPane tabId="1">
+            <Row>
+                <Col sm="12">
+                <h4>
+                    Tab 1 Contents
+                </h4>
+                </Col>
+            </Row>
+            </TabPane>
+            <TabPane tabId="2">
+            <Row>
+                <Col sm="6">
+                <Card body>
+                    <CardTitle>
+                    Special Title Treatment
+                    </CardTitle>
+                    <CardText>
+                    With supporting text below as a natural lead-in to additional content.
+                    </CardText>
+                    <Button>
+                    Go somewhere
+                    </Button>
+                </Card>
+                </Col>
+                <Col sm="6">
+                <Card body>
+                    <CardTitle>
+                    Special Title Treatment
+                    </CardTitle>
+                    <CardText>
+                    With supporting text below as a natural lead-in to additional content.
+                    </CardText>
+                    <Button>
+                    Go somewhere
+                    </Button>
+                </Card>
+                </Col>
+            </Row>
+            </TabPane>
+        </TabContent>
+        </div>
     );
 }
 
-function Home() {
-    return(
-        <div className="container">
-            <RenderTabs />
-        </div>
-    )
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = { isActive: false};
+    }
+
+    handleToggle = () => {
+        this.setState({ isActive: !this.state.isActive });
+        console.log(this.state.isActive);
+    }
+
+    render() {
+        const isActive = this.state.isActive;
+        return( 
+            <div className="mt-1 container">
+                <RenderTabs 
+                handleToggle={this.handleToggle}
+                isActive={isActive} />
+            </div>
+        );
+    }
+    
 }
 
 export default Home;
