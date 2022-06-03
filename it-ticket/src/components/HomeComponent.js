1473ed52efb779b6b2831600f2c8ca88b3e45957
Andrew <grozzy09@gmail.com>
2022-06-03 17:20:49 -0400

    Added Clickable Tabs

--- it-ticket/src/components/HomeComponent.js
+++ it-ticket/src/components/HomeComponent.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import tabData from '../data/tabData';
 import { Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, Card, CardTitle, CardText, Button} from 'reactstrap';
 import { FadeTransform } from 'react-animation-components';
 
-function RenderTabs({handleToggle, isActive}) {
+
+function RenderTabs({handleToggle, isSelected}) {
     return(
         <div className="row tab-shadow">
         <Nav justified tabs>
             <NavItem>
             <NavLink
-                className={isActive ? "active" : null}
+                className={isSelected === 1 || isSelected === 0 ? "active" : null}
+                id={"1"}
                 onClick={handleToggle}
             >
                 Dashboard
@@ -16,14 +19,33 @@ function RenderTabs({handleToggle, isActive}) {
             </NavItem>
             <NavItem>
             <NavLink
-                className={isActive ? null : "active"}
+                className={isSelected === 2 ? "active" : null}
+                id={"2"}
                 onClick={handleToggle}
             >
                 Tickets
             </NavLink>
             </NavItem>
+            <NavItem>
+            <NavLink
+                className={isSelected === 3 ? "active" : null}
+                id={"3"}
+                onClick={handleToggle}
+            >
+                Errors
+            </NavLink>
+            </NavItem>
+            <NavItem>
+            <NavLink
+                className={isSelected === 4 ? "active" : null}
+                id={"4"}
+                onClick={handleToggle}
+            >
+                Admin
+            </NavLink>
+            </NavItem>
         </Nav>
-        <TabContent className="mt-5" activeTab={isActive ? "1" : "2"}>
+        <TabContent className="mt-5" activeTab={isSelected ? "1" : "2"}>
             <TabPane tabId="1">
             <Row>
                 <Col sm="12">
@@ -68,24 +90,26 @@ function RenderTabs({handleToggle, isActive}) {
     );
 }
 
+
 class Home extends Component {
     constructor() {
         super();
-        this.state = { isActive: false};
+        this.state = { isSelected: 0};
     }
 
-    handleToggle = () => {
-        this.setState({ isActive: !this.state.isActive });
-        console.log(this.state.isActive);
+    handleToggle = (event) => {
+        this.setState({ isSelected: Number(event.target.id) });
+        console.log(this.isSelected);
+        
     }
 
     render() {
-        const isActive = this.state.isActive;
+        const isSelected = this.state.isSelected;
         return( 
             <div className="mt-2 container">
                 <RenderTabs 
-                handleToggle={this.handleToggle}
-                isActive={isActive} />
+                isSelected={isSelected}
+                handleToggle={this.handleToggle}/>
             </div>
         );
     }
